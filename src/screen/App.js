5d7244662171df343f0854ca13478071152ff49d
tarripewe <tarripewe01@gmail.com>
2022-07-31 18:55:58 +0700

    Initial Commit

---
+++ src/screen/App.js
@@ -0,0 +1,11 @@
+import Dashboard from "./dashboard/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/store-redux/store.js
@@ -0,0 +1,13 @@
+// import { createStore } from "redux";
+
+// const reducer = () => {};
+
+// const store = createStore(reducer);
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export default store;
