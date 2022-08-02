299cd7d1ca1375f0a5d7fa19f0e9e6a8e9d126b1
tarripewe <tarripewe01@gmail.com>
2022-08-02 19:27:17 +0700

    new design

--- src/screen/dashboard/Dashboard2.js
+++ src/screen/dashboard/Dashboard2.js
@@ -1,59 +1,59 @@
-import { Card, CardContent, Typography } from "@mui/material";
-import React, { useState } from "react";
-import { ActionButton } from "../../components/rootcomponents";
-
-const data = [
-  {
-    id: 1,
-    title: "TODO",
-    cards: [
-      {
-        id_cards: 1,
-        text: "nonton drakor",
-      },
-      {
-        id_cards: 2,
-        text: "nonton drakor",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "PROGRESS",
-    cards: [
-      {
-        id_cards: 1,
-        text: "nonton drakor",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "DONE",
-    cards: [
-      {
-        id_cards: 1,
-        text: "nonton drakor",
-      },
-    ],
-  },
-];
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Card, CardContent, Chip, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../../store-redux/features/postSlice";
 
 const Dashboard2 = () => {
+  const dispatch = useDispatch();
+
+  const { posts, loading } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <>
       <div style={styles.listContainer}>
-        {data.map((item) => (
+        {posts?.map((post) => (
           <div style={styles.container}>
-            <h4>{item.title}</h4>
-            {item.cards.map((index) => (
-              <Card style={styles.cardContainer}>
+            <div style={styles.header}>
+              <h4 key={post.id}>{post.title}</h4>
+              {post.title === "TO-DO" ? (
+                <button style={styles.addBtn}>ADD</button>
+              ) : (
+                ""
+              )}
+            </div>
+            {post?.todo.map((list) => (
+              <Card key={list.id} style={styles.cardContainer}>
                 <CardContent>
-                  <Typography gutterBottom>{index.text}</Typography>
+                  <Typography gutterBottom>{list.text}</Typography>
                 </CardContent>
+                <div style={{ marginLeft: 10, marginBottom: 5 }}>
+                  <Chip
+                    label={list.label}
+                    style={{
+                      backgroundColor:
+                        list.label === "Priority"
+                          ? "lightseagreen"
+                          : "" || list.label === "Medium"
+                          ? "yellowgreen"
+                          : "" || list.label === "Low"
+                          ? "pink"
+                          : "",
+                      color: "white",
+                      height: 20,
+                    }}
+                  />
+                </div>
               </Card>
             ))}
-            <ActionButton />
           </div>
         ))}
       </div>
@@ -76,6 +76,21 @@ const styles = {
   },
   cardContainer: {
     marginBottom: 8,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  addBtn: {
+    height: 30,
+    width: 70,
+    alignSelf: "center",
+    border: "none",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    cursor: "pointer",
+    fontWeight: "700",
   },
 };
 
--- src/store-redux/features/postSlice.js
+++ src/store-redux/features/postSlice.js
@@ -1,29 +1,14 @@
 /* eslint-disable no-const-assign */
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-const postID = 4;
 
 export const getPosts = createAsyncThunk("posts/getPosts", async () => {
-  return fetch("http://localhost:3000/posts").then((res) => res.json());
+  return fetch("http://localhost:3004/data").then((res) => res.json());
 });
-
-export const createPost = createAsyncThunk(
-  "posts/createPost",
-  async ({ id, title, cards }) => {
-    const response = await axios.get("http://localhost:3000/posts", {
-      id,
-      title,
-      cards,
-    });
-    return response.data;
-  }
-);
 
 const postSlice = createSlice({
   name: "posts",
   initialState: {
-    posts: [],
+    data: [],
     loading: false,
   },
   extraReducers: (builder) => {
@@ -37,13 +22,6 @@ const postSlice = createSlice({
     });
 
     builder.addCase(getPosts.rejected, (state, action) => {
-      state.loading = false;
-    });
-
-    builder.addCase(createPost.fulfilled, (state, action) => {
-      state.posts.id = postID += 1;
-      state.posts.title = action.payload.title;
-      state.posts.cards = [];
       state.loading = false;
     });
   },
